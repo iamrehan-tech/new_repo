7244fe369aa562482de58bfe6901134cefc07539
iamrehan-tech <rehanindia529@gmail.com>
2022-04-30 15:17:34 +0530

    modified

--- Cls.java
+++ Cls.java
@@ -1,7 +1,9 @@
 import java.util.*;
+import java.util.stream.StreamSupport;
 class Cls {
     public static void main(String[] args) {
         System.out.print("Hello world");
+        System.out.println("This is other files");
         
     }
 }
